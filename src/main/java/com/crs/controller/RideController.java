7260d9ff0a7607d6666fef34e008069852b8a477
Abishek Ravichandiran <abishekrabs@gmail.com>
2025-04-21 22:01:41 -0400

    Adding cors changes

--- src/main/java/com/crs/controller/RideController.java
+++ src/main/java/com/crs/controller/RideController.java
@@ -17,7 +17,6 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/rides")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:3000")
 public class RideController {
     private final RideService rideService;
 
--- src/main/java/com/crs/controller/UserController.java
+++ src/main/java/com/crs/controller/UserController.java
@@ -18,7 +18,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
     private final UserService userService;
 
